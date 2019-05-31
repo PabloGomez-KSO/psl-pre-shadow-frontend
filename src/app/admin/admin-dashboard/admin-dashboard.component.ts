3dd27b338a88863100d7f8f0f155f249dad87ea1
MrPauL91 <jupvillegasgo@unal.edu.co>
2019-05-31 15:19:02 -0500

    Added modification to create candidate table

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -4,12 +4,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css']
 })
-export class AdminDashboardComponent implements OnInit {
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
-
+export class AdminDashboardComponent {
 }
