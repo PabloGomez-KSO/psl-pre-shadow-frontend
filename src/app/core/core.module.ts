295e78fc9ed4714066dddcb9f124f8e45a3cf5f5
MrPauL91 <jupvillegasgo@unal.edu.co>
2019-06-12 17:05:47 -0500

    Added corrections according to tslint

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { AngularFireModule } from "@angular/fire";
-import { AngularFireAuthModule } from "@angular/fire/auth";
-import { AngularFireDatabaseModule } from "@angular/fire/database";
-import { AngularFireStorageModule } from "@angular/fire/storage";
-import { AngularFirestore } from "@angular/fire/firestore";
-import { AngularFireAuth } from "@angular/fire/auth";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
-//Components
-import { LoginComponent } from "./auth/login/login.component";
+// Components
+import { LoginComponent } from './auth/login/login.component';
 
-//Modules.
-import { AdminModule } from "../admin/admin.module";
+// Modules.
+import { AdminModule } from '../admin/admin.module';
 
-//Enviroment.
-import { environment } from "../../environments/environment";
-import { AuthGuard } from "./guards/auth.guard";
+// Enviroment.
+import { environment } from '../../environments/environment';
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   imports: [
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBRm71_zLZNUlPKn6Yl_5kQ8j7emILmq2w",
-    authDomain: "pre-shadow-psl-login.firebaseapp.com",
-    databaseURL: "https://pre-shadow-psl-login.firebaseio.com",
-    projectId: "pre-shadow-psl-login",
-    storageBucket: "pre-shadow-psl-login.appspot.com",
-    messagingSenderId: "900232660903",
-    appId: "1:900232660903:web:0304721568b15be2"
+    apiKey: 'AIzaSyBRm71_zLZNUlPKn6Yl_5kQ8j7emILmq2w',
+    authDomain: 'pre-shadow-psl-login.firebaseapp.com',
+    databaseURL: 'https://pre-shadow-psl-login.firebaseio.com',
+    projectId: 'pre-shadow-psl-login',
+    storageBucket: 'pre-shadow-psl-login.appspot.com',
+    messagingSenderId: '900232660903',
+    appId: '1:900232660903:web:0304721568b15be2'
   }
 };
